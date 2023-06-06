3aea8925bec4c063d153064dc146babeba5b4fb7
Allen Guo <guosonglin001@gmail.com>
2023-06-06 16:38:55 +0000

    add target_3_if.py

--- demos/slide/log.sh
+++ demos/slide/log.sh
@@ -3,3 +3,5 @@ python demos/slide/target_0_add.py
 python demos/slide/target_1_add_func.py
 
 python demos/slide/target_2_print.py
+
+python demos/slide/target_3_if.py
---
+++ demos/slide/target_3_if.py
@@ -0,0 +1,38 @@
+from typing import List
+import torch
+import torch._dynamo
+import torch._dynamo.config
+import logging
+
+torch._logging.set_logs(all=logging.DEBUG)
+torch._logging.set_logs(dynamo=logging.DEBUG)
+
+
+def my_compiler(gm: torch.fx.GraphModule, example_inputs: List[torch.Tensor]):
+    print("my_compiler() called with FX graph:")
+    # gm.graph.print_tabular()
+    return gm.forward
+
+
+@torch.compile(backend=my_compiler)
+def toy_example(a, b):
+    x = a / (torch.abs(a) + 1)
+    if b.sum() < 0:
+        b = b * -1
+    return x * b
+
+
+# # for _ in range(2):
+# for _ in range(1):
+#     toy_example(torch.randn(10), torch.randn(10))
+
+res = toy_example(torch.randn(10), torch.randn(10))
+print(res.shape)
+
+
+"""
+notice:
+
+POP_JUMP_IF_FALSE
+__resume_at_
+"""
