b68d06e3e29ee1bd79859f7bd8289d3e4e08e1ab
Allen Guo <guosonglin001@gmail.com>
2024-09-30 15:08:23 +0800

    wip

--- _demos/t0_build.sh
+++ _demos/t0_build.sh
@@ -1,8 +1,25 @@
+################################################################################
+
 git submodule update --recursive --init --depth=1
 
 # build with cmake
+# install with cmake
 
 # pip install -r requirements.txt
 
 pip install -e .
 # python setup.py develop
+
+################################################################################
+
+# cmake --preset osx -S. -Bbuild
+cmake --preset osx_allen -S. -Bbuild
+
+cmake --build build -t all
+
+# find build/lib/*.so -printf "%f\n" | xargs -d "\n" -I{} ln -s $PWD/build/lib/{} $PWD/torch/lib/{}
+# ln -s $PWD/build/functorch/functorch.so $PWD/functorch/functorch.so
+find build/lib/*.dylib -printf "%f\n" | xargs -d "\n" -I{} ln -s $PWD/build/lib/{} $PWD/torch/lib/{}
+ln -s $PWD/build/functorch/functorch.dylib $PWD/functorch/functorch.dylib
+
+################################################################################
