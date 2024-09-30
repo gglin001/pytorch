00bef9f22042f7f31730ed83088fb77c453fec43
Allen Guo <guosonglin001@gmail.com>
2024-09-30 16:24:07 +0800

    wip

--- _demos/t0.build.sh
+++ _demos/t0.build.sh
@@ -12,17 +12,29 @@ pip install -e . -vvv
 
 ################################################################################
 
+# git submodule update --recursive --init --depth=1
+git submodule update --init --depth=1
+git submodule update --init --recursive --depth 1 third_party/kineto
+git submodule update --init --recursive --depth 1 third_party/tensorpipe
+
+rm -rf build/CMakeCache.txt
+rm -rf build/CMakeFiles
+
 # cmake --preset osx -S. -Bbuild
 cmake --preset osx_allen -S. -Bbuild
 
 cmake --build build -t all
 
-# for zsh
-# micromamba install findutils
+# for osx
+micromamba install findutils
 
 # find build/lib/*.so -printf "%f\n" | xargs -d "\n" -I{} ln -s $PWD/build/lib/{} $PWD/torch/lib/{}
-# ln -s $PWD/build/functorch/functorch.so $PWD/functorch/functorch.so
 find build/lib/*.dylib -printf "%f\n" | xargs -d "\n" -I{} ln -s $PWD/build/lib/{} $PWD/torch/lib/{}
 ln -s $PWD/build/functorch/functorch.so $PWD/functorch/functorch.so
+
+################################################################################
+
+cmake --build build --target help
+cmake --build build --target help >_demos/cmake.target.log
 
 ################################################################################
